ae1c552ff3a649853ecce2d4f558af3e8d7d9fe9
Vignesh Illi <illivignesh1818@gmail.com>
2024-07-13 12:28:02 +0530

    Created file in Scrum31 Folder

---
+++ DSA-Task/SCRUM31/move-zeros-to-end.py
@@ -0,0 +1,16 @@
+class Solution:
+    def moveZeroes(self, nums):
+        last_non_zero_found_at = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[last_non_zero_found_at] = nums[i]
+                last_non_zero_found_at += 1
+        
+        for i in range(last_non_zero_found_at, len(nums)):
+            nums[i] = 0
+
+nums = [0, 1, 0, 3, 12]
+sol = Solution()
+sol.moveZeroes(nums)
+print(nums)
